09c3686ad1a801e67b8301f1d78fff435b2c7321
jeoninho623 <0623inho@naver.com>
2023-11-07 15:18:22 +0900

    spring 수업자료

---
+++ src/main/java/models/member/Amount.java
@@ -0,0 +1,10 @@
+package models.member;
+
+public class Amount {
+    private int total;  // 전체 결제 금액
+    private int tax_free;   // 비과세
+    private int vat;    // 부가세
+    private int point;  // 포인트
+    private int discount; // 할인
+
+}
